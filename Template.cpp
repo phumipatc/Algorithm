4a72430f10fcb05142d80c33f574f7a82375b400
Phumipat C. [MAGCARI] <55388114+phumipatc@users.noreply.github.com>
2021-10-26 18:44:24 +0700

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -9,9 +9,6 @@
 #define sz(x) (int)(x).size()
 #define MOD (LL)(1e9 + 7)
 using namespace std;
-using LL = long long;
-using PII = pair<int, int>;
-using PLL = pair<long long, long long>;
 const int dir4[2][4] = {{1, -1, 0, 0}, {0, 0, 1, -1}};
 const int dir8[2][8] = {{-1, -1, -1, 0, 1, 1, 1, 0}, {-1, 0, 1, 1, -1, 0, 1, -1}};
 
