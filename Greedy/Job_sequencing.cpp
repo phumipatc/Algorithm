10e4a18f3e5c26be4aa99a9cdb59ef6ce88aa7ba
PunnyOz2 <punnyoz1103@gmail.com>
2022-10-01 15:57:40 +0700

    add jobSequencingProfitSum

--- Greedy/Job_sequencing.cpp
+++ Greedy/Job_sequencing.cpp
@@ -1,5 +1,5 @@
 /*
-	Task		: Counting_sort
+	Task		: Job_sequencing
 	Author		: PunnyOz2
 	Language	: C++
 	Explanation	: 
@@ -37,11 +37,28 @@ vector<Job> jobSequencingResult(vector<Job> arr, int n) {
             pq.pop();
             slot_available--;
         }
-        sort(ans.begin(), ans.end(), [&](Job a, Job b) { return a.dead < b.dead; });
-    
-        // print the result
-        for (int i = 0; i < ans.size(); i++)
-            cout << ans[i].id << ' ';
-        cout << endl;
     }
+    sort(ans.begin(), ans.end(), [&](Job a, Job b) { return a.dead < b.dead; });
+    return ans;
+}
+
+int jobSequencingProfitSum(vector<Job> arr, int n) {
+    sort(arr.begin(), arr.end(), [](Job a, Job b) { return a.dead < b.dead; });
+    priority_queue<Job> pq;
+    int ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        int slot_available;
+        if (pq.empty()) {
+            slot_available = arr[i].dead;
+        } else {
+            slot_available = arr[i].dead - arr[i-1].dead;
+        }
+        pq.push(arr[i]);
+        while(!slot_available && !pq.empty()) {
+            ans += pq.top().profit;
+            pq.pop();
+            slot_available--;
+        }
+    }
+    return ans;
 }
