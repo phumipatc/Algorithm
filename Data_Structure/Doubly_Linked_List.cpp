900166c4de9fd1d25a78a26c2989ea9deec9b268
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2025-10-01 21:19:08 +0700

    fix: fix feedback

--- Data_Structure/Doubly_Linked_List.cpp
+++ Data_Structure/Doubly_Linked_List.cpp
@@ -1,16 +1,10 @@
 /*
-	Task		: Doubly Linkedin List
+	Task		: Doubly Linked List
 	Author		: Phumipat C.
 	Language	: C++
 	Explanation	: 
 */
-#include <bits/stdc++.h>
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-#define MOD (LL)(1e9 + 7)
-using namespace std;
-const int dir4[2][4] = {{1, -1, 0, 0}, {0, 0, 1, -1}};
-const int dir8[2][8] = {{-1, -1, -1, 0, 1, 1, 1, 0}, {-1, 0, 1, 1, -1, 0, 1, -1}};
+#include <iostream>
 
 struct Node{
 	int data;
